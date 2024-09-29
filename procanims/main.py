d48893294d10e3231082c3846f51bd26b462b155
Tsunami014 (Max) <Tsunami014@duck.com>
2024-09-29 10:50:44 +1000

    Added a polygon outline to the circles!

--- procanims/main.py
+++ procanims/main.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 pygame.init()
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((1000, 1000))
 
 class Point:
     def __init__(self, x, y, size):
@@ -21,8 +21,14 @@ class Point:
         ox, oy = opoint[0], opoint[1]
         if osize is None:
             osize = opoint[2]
-        phi = math.degrees(math.atan2(oy - self.y, ox - self.x))-180 % 360
-        self.x, self.y = ox+math.cos(math.radians(phi))*osize, oy+math.sin(math.radians(phi))*osize
+        phi = math.atan2(oy - self.y, ox - self.x)-math.pi
+        self.x, self.y = ox+math.cos(phi)*osize, oy+math.sin(phi)*osize
+    
+    def findOnCircle(self, angle):
+        return self.x+math.cos(math.radians(angle))*self.size, self.y+math.sin(math.radians(angle))*self.size
+    
+    def angleTo(self, opoint):
+        return math.degrees(math.atan2(opoint[1] - self.y, opoint[0] - self.x))-180 % 360
     
     def __getitem__(self, key):
         if key == 0:
@@ -48,7 +54,7 @@ class Point:
         return f"Point({self.x}, {self.y}, {self.size})"
     def __repr__(self): return str(self)
 
-ps = [Point(250, 250, 25), Point(250, 250, 25), Point(250, 250, 25), Point(250, 250, 25)]
+ps = [Point(250, 250, i) for i in [29, 35, 39, 52, 45]]
 
 clock = pygame.time.Clock()
 run = True
@@ -63,10 +69,36 @@ while run:
 
     ps[-1].pos = pygame.mouse.get_pos()
     for i in range(len(ps)-1):
-        ps[i].constrain(ps[i+1])
+        ps[i].constrain(ps[i+1])#, 25)#42)
     
     for p in ps:
         pygame.draw.circle(win, (255, 255, 255), p.pos, p.size, 4)
+    
+    newps = []
+    tmp = []
+    for i in range(len(ps)):
+        if i == len(ps) - 1:
+            ps2 = [90, 135, 180, -135, -90]
+            ang = ps[i - 1].angleTo(ps[i])
+        else:
+            ang = ps[i].angleTo(ps[i + 1])
+            if i == 0:
+                ps2 = [-90, -45, 0, 45, 90]
+            else:
+                ps2 = [90, -90]
+        for j in ps2:
+            newp = ps[i].findOnCircle(ang + j)
+            pygame.draw.circle(win, (255, 50, 50), newp, 2, 3)
+            if i == len(ps) - 1 or i == 0:
+                newps.append(newp)
+            else:
+                if j == 90:
+                    newps.append(newp)
+                else:
+                    tmp.append(newp)
+    newps.extend(tmp[::-1])
+    
+    pygame.draw.polygon(win, (10, 255, 50), newps, 10)
 
     pygame.display.update()
     clock.tick(60)
