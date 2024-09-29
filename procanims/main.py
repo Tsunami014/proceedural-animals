3175cf9f5bb957e3410aa8993ba7fb6407e9c4c4
Tsunami014 (Max) <Tsunami014@duck.com>
2024-09-29 11:25:51 +1000

    Kinda fixed the rounded polygon..?

--- procanims/main.py
+++ procanims/main.py
@@ -3,6 +3,61 @@ import math
 pygame.init()
 win = pygame.display.set_mode((1000, 1000))
 
+def average_angles(a1, a2):
+    # Convert angles to unit vectors
+    x1, y1 = math.cos(math.radians(a1)), math.sin(math.radians(a1))
+    x2, y2 = math.cos(math.radians(a2)), math.sin(math.radians(a2))
+
+    # Average the vectors
+    avg_x = (x1 + x2) / 2
+    avg_y = (y1 + y2) / 2
+
+    # Convert the resulting vector back to an angle
+    avg_angle = math.degrees(math.atan2(avg_y, avg_x))
+    
+    return avg_angle
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point clockwise by a given angle around a given origin.
+    The angle should be given in degrees.
+
+    Args:
+        origin: The point to rotate around
+        point: The point to rotate
+        angle: The angle to rotate around in degrees
+    """
+    angle = math.radians(angle)
+    cos = math.cos(angle)
+    sin = math.sin(angle)
+    ydiff = (point[1] - origin[1])
+    xdiff = (point[0] - origin[0])
+    
+    qx = origin[0] + cos * xdiff - sin * ydiff
+    qy = origin[1] + sin * xdiff + cos * ydiff
+    return qx, qy
+
+def draw_rounded_polygon(surface, colour, points, border_radius, width):
+    wrapped = points + [points[0]]
+    angs = [math.degrees(math.atan2(wrapped[i+1][1]-wrapped[i][1], wrapped[i+1][0]-wrapped[i][0]))-90 for i in range(len(wrapped)-1)]
+    for i in range(len(points)):
+        p1, p2 = points[i], points[i-1]
+
+        a = average_angles(angs[i], angs[i-1])-180
+        p1_start = rotate(p1, (p1[0], p1[1]-border_radius/2), angs[i-1])
+        p2_end = rotate(p2, (p2[0], p2[1]+border_radius/2), angs[i-1])
+        pygame.draw.line(surface, colour, p1_start, p2_end, width)
+        p = (
+            p1[0]+math.cos(math.radians(a))*border_radius,
+            p1[1]+math.sin(math.radians(a))*border_radius
+        )
+        a1, a2 = -angs[i]%360, -angs[i-1]%360
+        if (a2 - a1 + 180) % 360 - 180 > 180:
+            a1, a2 = a2, a1
+        pygame.draw.arc(surface, colour, 
+                        (p[0]-border_radius, p[1]-border_radius, border_radius*2, border_radius*2), 
+                        math.radians(a1), math.radians(a2), width)
+
 class Point:
     def __init__(self, x, y, size):
         self.x = x
@@ -70,62 +125,6 @@ while run:
     ps[-1].pos = pygame.mouse.get_pos()
     for i in range(len(ps)-1):
         ps[i].constrain(ps[i+1])#, 25)#42)
-    
-    for p in ps:
-        pygame.draw.circle(win, (255, 255, 255), p.pos, p.size, 4)
-    
-    def average_angles(a1, a2):
-        # Convert angles to unit vectors
-        x1, y1 = math.cos(math.radians(a1)), math.sin(math.radians(a1))
-        x2, y2 = math.cos(math.radians(a2)), math.sin(math.radians(a2))
-
-        # Average the vectors
-        avg_x = (x1 + x2) / 2
-        avg_y = (y1 + y2) / 2
-
-        # Convert the resulting vector back to an angle
-        avg_angle = math.degrees(math.atan2(avg_y, avg_x))
-        
-        return avg_angle
-
-    def rotate(origin, point, angle):
-        """
-        Rotate a point clockwise by a given angle around a given origin.
-        The angle should be given in degrees.
-
-        Args:
-            origin: The point to rotate around
-            point: The point to rotate
-            angle: The angle to rotate around in degrees
-        """
-        angle = math.radians(angle)
-        cos = math.cos(angle)
-        sin = math.sin(angle)
-        ydiff = (point[1] - origin[1])
-        xdiff = (point[0] - origin[0])
-        
-        qx = origin[0] + cos * xdiff - sin * ydiff
-        qy = origin[1] + sin * xdiff + cos * ydiff
-        return qx, qy
-    
-    def draw_rounded_polygon(surface, colour, points, border_radius, width):
-        wrapped = points + [points[0]]
-        angs = [math.degrees(math.atan2(wrapped[i+1][1]-wrapped[i][1], wrapped[i+1][0]-wrapped[i][0]))-90 for i in range(len(wrapped)-1)]
-        for i in range(len(points)):
-            p1, p2 = points[i], points[i-1]
-            a = average_angles(angs[i], angs[i-1])-180
-            pygame.draw.line(surface, colour, rotate(p1, (p1[0], p1[1]-border_radius), angs[i-1]), rotate(p2, (p2[0], p2[1]+border_radius), angs[i-1]), width)
-            p = (
-                p1[0]+math.cos(math.radians(a))*border_radius,
-                p1[1]+math.sin(math.radians(a))*border_radius
-            )
-            pygame.draw.arc(surface, 
-                            colour, 
-                            (p[0]-border_radius, p[1]-border_radius, border_radius*2, border_radius*2), 
-                            math.radians(-angs[i]), 
-                            math.radians(-angs[i-1]), 
-                            width
-                           )
     
     newps = []
     tmp = []
